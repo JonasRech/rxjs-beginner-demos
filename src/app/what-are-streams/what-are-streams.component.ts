51413ca6bc55302068cb019f05454a2bc847734a
Jonas Rech <info@jonasrech.io>
2020-07-27 15:12:22 +0000

    init

---
+++ src/app/what-are-streams/what-are-streams.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-what-are-streams',
+  template: `
+    <p>
+      what-are-streams works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class WhatAreStreamsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/what-are-streams/what-are-streams.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { WhatAreStreamsComponent } from './what-are-streams.component';
+
+
+
+@NgModule({
+  declarations: [WhatAreStreamsComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class WhatAreStreamsModule { }
